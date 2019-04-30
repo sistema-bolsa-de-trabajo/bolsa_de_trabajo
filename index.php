47832b76279eab9c6b7f976604e9b40ee5bfc493
Tiago <tiagoriver33@gmail.com>
2019-04-29 23:52:04 -0300

    creo el algoritmo principal del index

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
 
-echo 'este es el index';
+if (isset($_GET['view'])) {
+    include_once 'controlador/' . $_GET['view'] . '-controlador.php';
+} else {
+    include_once 'controlador/principal-controlador.php';
+}
